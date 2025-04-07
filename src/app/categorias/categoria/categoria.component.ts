c47bf0330e65abd40c34ecf32c17f6446515a25c
Kayro Silva <kayro.g.silva21@gmail.com>
2025-04-07 17:47:27 -0300

    61CriandoApi

---
+++ src/app/categorias/categoria/categoria.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-categoria',
+  standalone: false,
+  
+  templateUrl: './categoria.component.html',
+  styleUrl: './categoria.component.scss'
+})
+export class CategoriaComponent {
+  camposForm: FormGroup;
+
+  constructor(){
+    this.camposForm = new FormGroup({
+        nome: new FormControl('', Validators.required),
+        descricao: new FormControl('', Validators.required)
+    });
+  }
+
+  salvar(){
+    this.camposForm.markAllAsTouched();
+
+    if(this.camposForm.valid){
+      console.log('valores digitados: ', this.camposForm.value)
+    }
+  }
+
+  isCampoInvalido(nomeCampo: string) : boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return campo?.invalid && campo?.touched && campo?.errors?.['required']
+  }
+}
